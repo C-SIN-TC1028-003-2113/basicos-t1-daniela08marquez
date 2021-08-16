e6db7e6548645374083f9156cc1c8e6ced796dc1
daniela08marquez <A01740797@itesm.mx>
2021-08-16 23:46:18 +0000

    Tarea 1_A01740797

--- assignments/02AreaTriangulo/src/exercise.py
+++ assignments/02AreaTriangulo/src/exercise.py
@@ -1,7 +1,7 @@
 def main():
     #escribe tu código abajo de esta línea
-    n1 = int(input('Dame la base: '))
-    n2 = int(input('Dame la altura: '))
+    n1 = float(input('Dame la base: '))
+    n2 = float(input('Dame la altura: '))
 
     área = ( n1 * n2 )/ 2 
 
--- assignments/03Promedio/src/exercise.py
+++ assignments/03Promedio/src/exercise.py
@@ -1,9 +1,9 @@
 def main():
     #escribe tu código abajo de esta línea
-    n1 = int(input('Calificación de la materia: '))
-    n2 = int(input('Calificación de la materia: '))
-    n3 = int(input('Calificación de la materia: '))
-    n4 = int(input('Calificación de la materia: '))
+    n1 = float(input('Calificación de la materia: '))
+    n2 = float(input('Calificación de la materia: '))
+    n3 = float(input('Calificación de la materia: '))
+    n4 = float(input('Calificación de la materia: '))
 
     promedio = ( n1 + n2 + n3 + n4 ) / 4
 
