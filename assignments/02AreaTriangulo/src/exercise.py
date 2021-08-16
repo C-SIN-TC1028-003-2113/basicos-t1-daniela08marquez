89adb937c55923a79c7eaa83fbd3a8d6a84d2fc1
daniela08marquez <A01740797@itesm.mx>
2021-08-16 23:24:27 +0000

    Act3Clase_Daniela_Marquez

--- assignments/02AreaTriangulo/src/exercise.py
+++ assignments/02AreaTriangulo/src/exercise.py
@@ -1,6 +1,11 @@
 def main():
     #escribe tu código abajo de esta línea
-    pass
+    n1 = int(input('Dame la base: '))
+    n2 = int(input('Dame la altura: '))
+
+    área = ( n1 * n2 )/ 2 
+
+    print('El área es: '+str(área))
 
 if __name__ == '__main__':
     main()
--- assignments/03Promedio/src/exercise.py
+++ assignments/03Promedio/src/exercise.py
@@ -1,6 +1,13 @@
 def main():
     #escribe tu código abajo de esta línea
-    pass
+    n1 = int(input('Calificación de la materia: '))
+    n2 = int(input('Calificación de la materia: '))
+    n3 = int(input('Calificación de la materia: '))
+    n4 = int(input('Calificación de la materia: '))
+
+    promedio = ( n1 + n2 + n3 + n4 ) / 4
+
+    print('El promedio es: '+str(promedio))
 
 
 if __name__ == '__main__':
